d10d7cc75dd70ede56d0fb0971d5f46c4c75ad1b
JimmyJohn1026 <jmillion627@gmail.com>
2023-03-13 20:47:44 -0400

    switch to godot 4

--- autoload/global.gd
+++ autoload/global.gd
@@ -1,26 +1,14 @@
 extends Node
 
 
-onready var player: KinematicBody2D = get_node("/root/Main/Player")
-onready var instructions: Instructions = get_node("/root/Main/CanvasLayer/HUD/Instructions")
-
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	Engine.set_target_fps(60)
+
 
 func _input(event):
 	if event.is_action_pressed("fullscreen"):
-		OS.set_window_fullscreen(!OS.window_fullscreen)
+		pass
 	if event.is_action_pressed("attack"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if event.is_action_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-
-func freeze_frame(time_scale: float, duration: float):
-	Engine.time_scale = time_scale
-	yield(get_tree().create_timer(time_scale * duration), "timeout")
-	Engine.time_scale = 1.0
-
-func screen_shake(amount: float, max_trauma: float):
-	player.get_node("Camera2D").add_trauma(amount, max_trauma)
--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,6 @@
-extends KinematicBody2D
+extends Character
 
 
-var current_state: int = State.NORMAL
 enum State {
 	NORMAL,
 	ATTACK,
@@ -9,15 +8,10 @@ enum State {
 	HOLD,
 	THROW,
 }
+var current_state := State.NORMAL
 
-var velocity := Vector2.ZERO
-var max_speed := 400
-var acceleration := 100
-var player_direction = Vector2.RIGHT
-
-var holding: Node2D
-var throw_force = 1400
-var throw_height = 100
+var max_speed := 300
+var acceleration := 50
 
 
 func _physics_process(delta):
@@ -29,101 +23,70 @@ func _physics_process(delta):
 		State.GRAB:
 			grab()
 		State.HOLD:
-			hold(delta)
+			hold()
 		State.THROW:
 			throw()
 
 func normal():
-	move()
-	$AnimationPlayer.play("idle")
-	
 	if Input.is_action_just_pressed("attack"):
 		current_state = State.ATTACK
-	if Input.is_action_just_pressed("grab"):
+		var hits = %AttackBox.get_overlapping_areas()
+		if hits:
+			for hit in hits:
+				hit.owner.damage(self, 2, %AttackOrigin.global_position.direction_to(hit.owner.position) * 400, -400)
+		attack()
+		return
+	elif Input.is_action_just_pressed("grab"):
 		current_state = State.GRAB
+		grab()
+		return
+	
+	move()
+	$AnimationPlayer.play("idle" + direction_string())
+
 
 func attack():
-	$AnimationPlayer.play("attack")
 	move()
-
-func do_damage():
-	var hits = $AttackBox.get_overlapping_areas()
-	if hits:
-		for hit in hits:
-			hit.owner.damage(self, 2, $AttackOrigin.global_position.direction_to(hit.owner.position) * 400, -400)
+#	$AnimationPlayer.play("")
 
 func grab():
-	$AnimationPlayer.play("grab")
 	move()
+#	$AnimationPlayer.play("grab")
 
-func do_grab():
-	var overlap = $AttackBox.get_overlapping_areas()
-	if overlap:
-		Global.instructions.grabbed = true # for the tutorial guided
-		holding = overlap[0].owner
-		holding.held = true
-		current_state = State.HOLD
-
-func hold(delta):
-	$AnimationPlayer.play("holding")
-	move()
-	
-	# where the shadow goes
-	holding.global_position = $HoldOrigin.global_position
-	# where the sprite goes
-	holding.get_node("SpriteOrigin").position.y = $HoldOrigin/SpriteOrigin.position.y
-	
-#	var magnitude = (player_direction * throw_force + velocity).length() + throw_height
-#	var throw_distance = get_range(holding.gravity, magnitude, asin(throw_height / magnitude), delta)
-#	$Node/Target.position = (throw_distance - 100) * player_direction + $HoldOrigin.position + position
-	
-	if Input.is_action_just_pressed("attack"):
-		current_state = State.THROW
-	elif Input.is_action_just_pressed("grab"): # let go of enemy
-		holding.held = false
-		holding.vertical_velocity = 0.0
-		current_state = State.NORMAL
-
-#func get_range(gravity, force, angle, delta):
-#	return pow(force, 2) * sin(2 * angle) / gravity * delta
+func hold():
+	pass
 
 func throw():
-	$AnimationPlayer.play("throw")
-	move()
+	pass
 
-func do_throw():
-	Global.instructions.thrown = true # for the tutorial instructions
-	holding.throw(player_direction * throw_force + velocity, -throw_height)
-	holding.throw(player_direction * throw_force, -throw_height)
-	holding.held = false
-
-
-func transition_to(new_state: int):
-	current_state = new_state
 
 func move():
 	var goal = get_input() * max_speed # the desired direction
 	var to_goal = goal - velocity # direction from velocity to desired direction
 	var accel_speed = min(acceleration, to_goal.length()) # clamp the speed
 	var steering = to_goal.normalized() * accel_speed # return the movement
-	velocity = move_and_slide(velocity + steering)
-	do_flip()
+	velocity += steering
+	move_and_slide()
+#	do_flip()
 
-func do_flip():
-	var input = get_input()
-	if input.x < 0: # to the left
-		$Sprite.flip_h = true
-	if input.x > 0: # to the right
-		$Sprite.flip_h = false
-	
-	if $Sprite.flip_h: # to the left
-		$AttackBox.position.x = -abs($AttackBox.position.x)
-		$AttackOrigin.position.x = -abs($AttackOrigin.position.x)
-		$HoldOrigin.position.x = -abs($HoldOrigin.position.x)
-	else: # to the right
-		$AttackBox.position.x = abs($AttackBox.position.x)
-		$AttackOrigin.position.x = abs($AttackOrigin.position.x)
-		$HoldOrigin.position.x = abs($HoldOrigin.position.x)
+#func do_flip():
+#	var input = get_input()
+#	if input.x < 0: # to the left
+#		$SpriteOrigin/Sprite2D.flip_h = true
+#	if input.x > 0: # to the right
+#		$SpriteOrigin/Sprite2D.flip_h = false
+#
+#	if $SpriteOrigin/Sprite2D.flip_h: # to the left
+#		%AttackBox.position.x = -abs(%AttackBox.position.x)
+#		%AttackOrigin.position.x = -abs(%AttackOrigin.position.x)
+#		%HoldOrigin.position.x = -abs(%HoldOrigin.position.x)
+#	else: # to the right
+#		%AttackBox.position.x = abs(%AttackBox.position.x)
+#		%AttackOrigin.position.x = abs(%AttackOrigin.position.x)
+#		%HoldOrigin.position.x = abs(%HoldOrigin.position.x)
+
+func transition_to(new_state: State):
+	current_state = new_state
 
 func get_input() -> Vector2: # returns normalized direction input
 	var input = Vector2.ZERO
@@ -138,6 +101,6 @@ func get_input() -> Vector2: # returns normalized direction input
 		input.x += 1
 	
 	if input:
-		player_direction = input.normalized()
+		direction = input.normalized()
 	
 	return input.normalized()
